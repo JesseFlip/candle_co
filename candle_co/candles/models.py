98b06c2c95fefeae5d635761f7aab1b9341d48b2
JesseFlip <jss.flppn@gmail.com>
2025-06-12 22:39:42 -0500

    Added database

--- candle_co/candles/models.py
+++ candle_co/candles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Candles(models.Model):
+    name = models.CharField(max_length=64)
+    scent = models.CharField(max_length=64)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    created_at = models.DateTimeField(default=date.today)
+
+    def __str__(self):
+        return f"{self.name} - {self.scent} ${self.price} In Stock:{self.inventory} Created on:{self.created_at}"
--- candle_co/candles/views.py
+++ candle_co/candles/views.py
@@ -1,28 +1,11 @@
-#from django.shortcuts import render
-import json
-from django.http import HttpResponse
+from django.shortcuts import render
+#import json
+#from django.http import HttpResponse
+from .models import Candles
 
 # Create your views here.
 def index(request):
-    data = {
-        "candle_1": {
-            "name": "Cool Breeze",
-            "price": 14.99,
-            "scent": "mint",
-            "category": "refreshing"
-        },
-        "candle_2": {
-            "name": "Salted Amber",
-            "price": 14.99,
-            "scent": "Amber",
-            "category": "invigorating"
-        },
-        "candle_3": {
-            "name": "Cocoa Butter",
-            "price": 14.99,
-            "scent": "Cocoa Butter",
-            "Category": "natural"
-        } 
-    }
-    response = HttpResponse(json.dumps(data), content_type='application/json')
-    return response
+    return render(request, 'candles/index.html', {
+        'candles': Candles.objects.all()
+    })
+    
