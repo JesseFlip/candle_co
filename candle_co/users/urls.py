045ed3daa9598b5e32ad7a78fbb450bf8698b1a0
JesseFlip <jss.flppn@gmail.com>
2025-06-15 17:20:14 -0500

    candle route working

--- candle_co/users/urls.py
+++ candle_co/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns -[
+urlpatterns = [
     path("", views.index, name='index'),
-    path("login", views.login, name='login'),
-    path("logout", views.logout, name='logout')
+    path("login/", views.login_view, name='login'),
+    path("logout/", views.logout_view, name='logout')
 ]
--- candle_co/users/views.py
+++ candle_co/users/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    
+def login_view(request):
+    if request.methon == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    pass
+    #return render(request, "users/logout.html")
